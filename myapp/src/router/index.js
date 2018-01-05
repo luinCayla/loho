5dae6c5729efde90d2c816d6d8b8e98ae6ed94dc
huaixu5421 <fengzhe5421@163.com>
2018-01-05 23:36:36 +0800

    change router 2

--- myapp/src/router/index.js
+++ myapp/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/home'
 import GroupBuying from '@/components/groupbuying'
 import Service from '@/components/Service'
 import NearbyStore from '@/components/NearbyStore'
