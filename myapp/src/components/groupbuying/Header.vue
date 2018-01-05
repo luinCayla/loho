60444b32ce638243b0475aa59c31a746824d59c7
huaixu5421 <fengzhe5421@163.com>
2018-01-05 22:51:55 +0800

    初步完成了团购页面的编写

---
+++ myapp/src/components/groupbuying/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div  id="g-head" >
+      
+       <h2 class="head-title">团购分类</h2>
+        <div class="head-ctrl-left">
+           <a class=" iconfont icon-left"></a>
+       </div>
+       <div @click='changeType'  class="head-ctrl-right">
+           <a  class=" iconfont icon-caidan"></a>
+       </div>
+            
+
+    </div>
+     
+</template>
+
+ 
+<script>
+    import bus from '../../module/bus'
+    export default {
+       name: 'g-header',
+       data () {
+            return {
+                
+            }
+        },
+        methods:{
+            changeType(){           
+                bus.$emit("change-type")
+            }
+        } 
+    }
+
+
+</script>
+
+<style  scoped>
+
+
+</style>
---
+++ myapp/src/components/groupbuying/Hide.vue
@@ -0,0 +1,36 @@
+<template>
+    <nav class="head-menu cl" style="display: block;" v-if="isShow">
+        <a class = "icon-home3 iconfont icon-house"  href="">首页</a>
+        <a class = "icon-user iconfont icon-member"  href="">会员中心</a>
+        <a class = "icon-cart iconfont icon-shopping"  href="">购物车</a>
+        <a class = "icon-comment iconfont icon-information"  href="">在线咨询</a>
+    </nav> 
+            
+
+</template>
+
+<script>
+    import bus from '../../module/bus'
+    export default {
+        name: 'group-hide',
+        data () {
+            return {
+                isShow:false
+            }
+        },
+        components:{
+
+        },
+       mounted(){
+           bus.$on('change-type',function(){
+               this.isShow = !this.isShow;
+           }.bind(this))
+       }
+    }
+
+
+</script>
+
+<style  scoped>
+  
+</style>
