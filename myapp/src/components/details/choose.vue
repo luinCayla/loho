7ce21164e088abfc13b7f87744659bb128568bba
luinCayla <971030937@qq.com>
2018-01-11 12:25:08 +0800

    detail中img的动态获取

--- myapp/src/components/details/choose.vue
+++ myapp/src/components/details/choose.vue
@@ -7,7 +7,7 @@
             <a >商品价格</a>
             <a >商品参数</a>
         </div>
-        <div class="txtcontent" v-html="txtcontent"></div>
+        <div class="txtcontent" v-html="text"></div>
 
         <div style="text-align:center;height:.42rem;line-height:.42rem;">------看了又看------</div>
         <div style="text-align:center;height:.42rem;line-height:.42rem;color:gray;margin-bottom:.6rem">------已经到底了------</div>
@@ -27,24 +27,19 @@ export default {
     methods:{
       
     },
-    mounted(){
-    
-        var str = this.txtcontent;       
+    computed:{
+        text(){
         //http://image.loho88.com/images/ueditor/20161017/1476677265163940.jpg
-        let imgurl = str.substr(0,12)+'http://image.loho88.com/'+str.substr(40,);
+            let txtcontent = this.txtcontent;
+            txtcontent = txtcontent.replace(/src="\/images\/blank.gif"/g,"");
 
-         console.log(str)
-         //<p><img src="/images/blank.gif" vip-src="/images/ueditor/20171227/1514372026687401.jpg" title="1514372026687401.jpg" alt="02.jpg"/><img src="/images/blank.gif" vip-src="/images/ueditor/20171227/1514370858353825.jpg" style="" title="1514370858353825.jpg"/></p>
-         //<p><img src="/images/blank.gif" vip-src="/images/ueditor/20171227/1514370858113426.jpg" style="" title="1514370858113426.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370858456574.jpg" style="" title="1514370858456574.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370859136501.jpg" style="" title="1514370859136501.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370859499729.jpg" style="" title="1514370859499729.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370859630793.jpg" style="" title="1514370859630793.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370859864259.jpg" style="" title="1514370859864259.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370859239474.jpg" style="" title="1514370859239474.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370860996153.jpg" style="" title="1514370860996153.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370860661787.jpg" style="" title="1514370860661787.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370860642117.jpg" style="" title="1514370860642117.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370860123013.jpg" style="" title="1514370860123013.jpg"/></p><p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370861458656.jpg" style="" title="1514370861458656.jpg"/></p>
-         //<p><img src="/images/blank.gif" data-src="/images/ueditor/20171227/1514370861246523.jpg" style="" title="1514370861246523.jpg"/></p>
-         //<p><br/></p>
+            txtcontent = txtcontent.replace(/vip-src="\/images\//g,'src="http://image.loho88.com/images/');
+            txtcontent = txtcontent.replace(/data-src="\/images\//g,'src="http://image.loho88.com/images/');
 
+            return txtcontent
+        }
     }
+
 
 }
 </script>
